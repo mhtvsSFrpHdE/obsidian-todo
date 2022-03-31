cfe5006c44eda4dafb63e7b990fcad194f2e400f
mhtvsSFrpHdE <2bemperor47@gmail.com>
2022-03-31 10:21:03 +0800

    Update config

--- Config/ObsidianFilePathExample.py
+++ Config/ObsidianFilePathExample.py
@@ -1,5 +1,8 @@
+# Obsidian vault name
+# Must opened manually at least once
 vault = "VaultName"
 
+# Weekday notes
 monday = "daily/mon.md"
 tuesday = "daily/tue.md"
 wednesday = "daily/wed.md"
@@ -8,6 +11,14 @@ friday = "daily/fri.md"
 saturday = "daily/sat.md"
 sunday = "daily/sun.md"
 
+# Everyday notes, absolute path
+everyday = r"C:\everyday.md"
+everydayTemplate = r"C:\everyday.md.template"
+
+# Last run file path
 lastRun = "lastRun"
 
+# Do not modify
+
+# Map weekday notes to datetime weekday index
 weekArray = [monday, tuesday, wednesday, thursday, friday, saturday, sunday]
